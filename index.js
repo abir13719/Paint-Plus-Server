ee998664496ac7a5c510655974f87b507a32ba1b
abir13719 <abir13719@gmail.com>
2024-05-02 21:43:37 +0600

    Slider and My Paint Database Created

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8que7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8que7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb://localhost:27017";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const SliderCollection = client.db("paintPlus").collection("slider");
+    const PaintCollection = client.db("paintPlus").collection("paintings");
+    app.get("/slider", async (req, res) => {
+      const cursor = SliderCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/painting", async (req, res) => {
+      const painting = req.body;
+      const result = await PaintCollection.insertOne(painting);
+      res.send(result);
+    });
+
+    app.get("/painting/:email", async (req, res) => {
+      const cursor = PaintCollection.find({ email: req.params.email });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
